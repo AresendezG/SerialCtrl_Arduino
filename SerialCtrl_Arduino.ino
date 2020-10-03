b1f9d6d4ec8c0e43c12dd5532a5c13addc4ad8a3
Alejandro Resendez <al.resendez.gro@gmailcom>
2020-10-02 23:23:36 -0500

    Parse the string input works now

--- SerialCtrl_Arduino.ino
+++ SerialCtrl_Arduino.ino
@@ -5,13 +5,26 @@
 
 #include <String.h>
 
+const String UserMessage = "Enter your Command: >"; // Message for the COM user 
+const String ErrorMessage = "ERROR: Wrong Sintax \n";
 String inputString = "";         // a String to hold incoming data
-String cmd = "";
-String remain = "";
+String cmd = ""; // String to hold the command in the Input String 
+String port = "";
 String operation = "";
-uint8_t cmdIndex;
+String remain = "";
+
+int cmdIndex;
+                // D12 D2 D3 D4 D5 D6 D7 D8 D9 D10 D11     
+char GPIOs [11] = {12, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}; // Arduino Nano GPIO's
 
 void setup() {
+
+  // Set all GPIO Pins as INPUT_PULLUP 
+
+    for (int i=0; i<11; i++) {
+      pinMode(GPIOs[i], INPUT_PULLUP);
+    }
+
   //Enable built In LED ctrl 
   pinMode(LED_BUILTIN, OUTPUT);
   // initialize serial:
@@ -19,15 +32,16 @@ void setup() {
   inputString.reserve(200);
   remain.reserve(100);
   cmd.reserve(50);
-  Serial.println("Enter your command: > ");
-  Serial.setTimeout(2500); // This timeout might not be important? 
+  port.reserve(50);
+  operation.reserve(50);
+  Serial.println(UserMessage);
+  Serial.setTimeout(500); // This timeout might not be important? 
 
 }
 
 
-uint8_t matchPattern(String inStr, String *beforeStr, String *afterStr, char chrToFind){
-  uint8_t index;
-
+int matchPattern(String inStr, String *beforeStr, String *afterStr, char chrToFind){
+    int index;
     index = inStr.indexOf(chrToFind);
       if (index > 0){
           *beforeStr = inStr.substring(0,index);
@@ -38,9 +52,8 @@ uint8_t matchPattern(String inStr, String *beforeStr, String *afterStr, char chr
       {
         return -1; // Char not found in string
       }
-      
-
 }
+
 
 //In this function, it will convert certain types of strings into TRUE or FALSE values
 bool strToBol(String inStr){
@@ -53,39 +66,88 @@ bool strToBol(String inStr){
 
 void loop() {
   
-
-  // Read a string from the serial Port 
+  // Read a String from the Serial Port 
   inputString = Serial.readString();
   if (inputString != ""){
-    
-    // find the command 
+    // Find the command 
      cmdIndex =  matchPattern(inputString, &cmd, &remain,' ');
-
         if(cmdIndex >0)
         { 
-          Serial.println("command found: ");
-            if (cmd.compareTo("led")){
+          Serial.print("Echo the received Command: ");
+          Serial.print(cmd);
+          Serial.print("\n"); //Echo the received command
+
+            if (cmd.equalsIgnoreCase("led")){
                 
-                if (remain.equalsIgnoreCase("on\n") || remain.equalsIgnoreCase("off\n")) // This one checks if the sintax is valid
+                if (remain.equalsIgnoreCase("on\n") || remain.equalsIgnoreCase("off\n")) // This one checks if the operation Option is valid
                 {
                     Serial.print("Set LED to:");
-                    Serial.print(cmd);
+                    //Serial.print(cmd);
                     Serial.println(remain);
                     digitalWrite(LED_BUILTIN, strToBol(remain));
+                    //Serial.println("Enter Command >");
                 }
-                else
+                else // Wrong Operation Mode 
                 {
-                  Serial.println("Wrong Sintax! ");
+                  Serial.println(ErrorMessage);
                 }
-                
-
             }
-            else if(cmd.compareTo("read")){
+            else if(cmd.equalsIgnoreCase("read")){ // Read the Status of a Port
+
+               cmdIndex = matchPattern(remain, &port, &operation, ' '); // Will find Selected port and Selected Operation
+               int PortNum = port.toInt();
+
+               if (PortNum > 0 && PortNum < 11) { // Only if Pin# Is correct, 
+                    Serial.print("Port: ");
+                    Serial.print(port); 
+                    // Verify the Digital Read 
+                    bool PinStatus = digitalRead(GPIOs[PortNum]);
+                  
+                  if (PinStatus == true){
+                    Serial.print(" is HIGH \n");
+                  } 
+                  else
+                  {
+                    Serial.print(" is LOW \n");
+                  }
+               
+               }
+               else { // Wrong Port number, Return Error Flag
+                  Serial.print("You Selected Port#: ");
+                  Serial.print(port);
+                  Serial.print("\n");
+                  Serial.println("ERROR: Wrong Port Number, Pick 1 to 10 \n");
+               }
+
 
             }
             
-            else if(cmd.compareTo("set")){
+            else if(cmd.equalsIgnoreCase("set")){ // Set a port into ON or OFF state  
+                cmdIndex = matchPattern(remain, &port, &operation, ' '); // Will find Selected port and Selected Operation
+               //   Serial.print("Index for Port: ");                
+               //   Serial.print(cmdIndex);
+               //   Serial.print("\n");
+                /* Parse the Input String: */
+                Serial.print("Command: ");
+                Serial.print(cmd);
+                Serial.print("\t Port: ");
+                Serial.print(port);
 
+                int portNumeric = port.toInt();
+
+                Serial.print("\t Operation: ");
+                Serial.print(operation);
+                Serial.println("");
+
+                  if ((operation.substring(0,1)).equalsIgnoreCase("on")){
+
+                  }
+                  else if ((operation.substring(0,2)).equalsIgnoreCase("off")) {
+                      // Default 
+                  }
+                  else {
+                    Serial.println(ErrorMessage);
+                  }
             }
 
             else {
@@ -94,19 +156,17 @@ void loop() {
         }
         else
         {
-          Serial.println("Wrong Sintax");
+          Serial.println(ErrorMessage);
         }
         
     //Serial.println(cmd); //Echo back
     inputString=""; //Reset InputStr variable to pick a new instruction 
+    Serial.println(UserMessage); 
     delay(200);
-
+    
   }
   else {
-    Serial.println("Enter your command >");
+    // Serial.println("Enter your command >");
   }
 
 }
-
-
-
